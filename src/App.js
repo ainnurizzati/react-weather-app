1a7391c1f58d2a591d98f4d772ebf143f94a3f19
Ain Nurizzati <ainnurizzati24@gmail.com>
2022-03-12 16:24:19 +0800

    Added lat and long from navigation.geolocation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(()=> {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    // await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid={API key}`)
+    //   .then (response => console.log(response));
+
+  }, [lat, long]);
+
+
+
   return (
     <div className="App">
       <p>Hello</p>
